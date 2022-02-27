7ede86a159f6ed00d26febb0e2f586b52ef0eda8
Matt Draghi <mtd5264@gmail.com>
2022-02-27 16:48:22 -0500

    commit tree 7a3da8591d10d1db255da0e3af3d7852a2bcfc53

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+require_relative './user'
 
+class Student < User
+    attr_accessor :knowledge
+
+    def initialize
+        self.knowledge = []
+    end
+
+    def learn(string)
+        @knowledge.append(string)
+    end
 end
